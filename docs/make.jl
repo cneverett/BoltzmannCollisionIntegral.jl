0717bcb793fcc80e31a20a9ff84a09a3dde46c1e
Christopher Everett <cn.everett@outlook.com>
2024-07-18 15:20:51 +0100

    + attempt to fix MathJax3

--- docs/make.jl
+++ docs/make.jl
@@ -5,20 +5,23 @@ using Documenter
 makedocs(
          sitename = "BinaryInteractionSpectra.jl",
          modules  = [BinaryInteractionSpectra],
-         mathengine = MathJax3(Dict(
-            :loader => Dict("load" => ["[tex]/physics"]),
-            :tex => Dict(
-                "inlineMath" => [["\$","\$"], ["\\(","\\)"]],
-                "tags" => "ams",
-                "packages" => ["base", "ams", "autoload", "physics"],
-            ),
-            )),
          pages=[
                 "Overview" => "index.md",
                 "Getting Started" => "quickstart.md",
                 "Cross Sections" => "crosssections.md",
                 "Internal Functions" => "internalfunctions.md"
-               ])
+               ],
+        format = Documenter.HTML(
+            mathengine = MathJax3(Dict(
+                :loader => Dict("load" => ["[tex]/physics"]),
+                :tex => Dict(
+                    "inlineMath" => [["\$","\$"], ["\\(","\\)"]],
+                    "tags" => "ams",
+                    "packages" => ["base", "ams", "autoload", "physics"],
+                ),
+            ))
+        ),
+        )
 deploydocs(;
     repo="github.com/cneverett/BinaryInteractionSpectra.jl",
 )
