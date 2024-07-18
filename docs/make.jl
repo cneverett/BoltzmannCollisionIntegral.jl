4993e58de410b10e4df55ada8c2410a18427bcc8
Christopher Everett <cn.everett@outlook.com>
2024-07-18 15:17:31 +0100

    + change latex engine to MathJax3

--- docs/make.jl
+++ docs/make.jl
@@ -5,6 +5,14 @@ using Documenter
 makedocs(
          sitename = "BinaryInteractionSpectra.jl",
          modules  = [BinaryInteractionSpectra],
+         mathengine = MathJax3(Dict(
+            :loader => Dict("load" => ["[tex]/physics"]),
+            :tex => Dict(
+                "inlineMath" => [["\$","\$"], ["\\(","\\)"]],
+                "tags" => "ams",
+                "packages" => ["base", "ams", "autoload", "physics"],
+            ),
+            )),
          pages=[
                 "Overview" => "index.md",
                 "Getting Started" => "quickstart.md",
