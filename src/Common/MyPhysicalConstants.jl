0450d2bc55ab246b8d837bf7280ec722cffa4c64
Christopher Everett <cn.everett@outlook.com>
2024-07-22 11:53:23 +0100

    - ParticleData.jl - MyPhysicalConstants.jl, surplanted with Constants.jl

--- src/Common/MyPhysicalConstants.jl
+++
@@ -1,7 +0,0 @@
-# Physical constants taken from https://physics.nist.gov/cuu/Constants/index.html
-
-const c::Float32 = Float32(299792458); # m s-1
-const σT::Float32 = Float32(6.6524587e-29); # m2
-  
-# sphere radius used for hard sphere collisions 
-const RSph::Float32 = Float32(1e-15); # m
