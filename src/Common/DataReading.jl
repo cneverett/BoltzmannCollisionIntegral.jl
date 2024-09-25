6ea7a99181eed9c5afb40516dff69cb2d808cca0
Christopher Everett <cn.everett@outlook.com>
2024-09-25 17:21:36 +0100

    + minor docstring update

--- src/Common/DataReading.jl
+++ src/Common/DataReading.jl
@@ -189,7 +189,7 @@ end
 
 
 """
-    DoesConserve(SMatrix,TMatrix,Parameters)
+    DoesConserve(SMatrix3,SMatrix4,TMatrix1,TMatrix2,Parameters)
 
 Function prints the ratio of the sum of the S and T matricies and their differences, for all interaction paths, as to check number and energy conservation for a particular interaction. Arguments are as outputted by the `fload_All` function. 
 """
