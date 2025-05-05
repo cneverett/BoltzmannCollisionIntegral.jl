9403c83b410aa6e4685e86a70236f5e0a731df59
Christopher Everett <cn.everett@outlook.com>
2025-05-05 16:52:55 +0100

    + weighting factors

--- src/Common/RandomPoints.jl
+++ src/Common/RandomPoints.jl
@@ -31,7 +31,7 @@ end
 Assigns the second (cos(theta)) and third (phi) elements of 'a' with a randomly, uniformly sampled values of spherical angles cos(theta) and phi (phi normalised by pi). 
 """
 function RPointSphereCosThetaPhi!(a::Vector{Float64}) 
-    # Inputs a 3 element vector [p, cos(theta), phi] and mutates said vector with new random values using form given in https://mathworld.wolfram.com/SpherePointPicking.html (with theta and phi changed places)
+    # Inputs a 4 element vector [p, cos(theta), phi/pi,theta/pi] and mutates said vector with new random values using form given in https://mathworld.wolfram.com/SpherePointPicking.html (with theta and phi changed places)
     # phi points are normalised by pi
 
     u::Float64 = rand(Float64)
@@ -39,6 +39,7 @@ function RPointSphereCosThetaPhi!(a::Vector{Float64})
 
     a[2] = 2*v-1     # cos(theta) bound by [1,-1]
     a[3] = 2*u       # phi bound by [0,2) 
+    a[4] = acos(a[2])/pi # theta bound by [0,1]
 
     return nothing
     
@@ -151,4 +152,70 @@ function pdfBoost(βv::Vector{Float64},ctC::Float64)
     
     return prob
 
+end
+
+
+"""
+    WeightedFactors!()
+
+Returns the weighting rapidity `w` and the direction an angles `t` and `h` for rotations on a sphere. 
+Weighting rapidity can be scaled by `scale`.
+"""
+function WeightedFactors(p1v::Vector{Float64},p2v::Vector{Float64},m1::Float64,m2::Float64,scale::Float64) 
+
+    E1::Float64 = sqrt(p1v[1]^2+m1^2)
+    E2::Float64 = sqrt(p2v[1]^2+m2^2)
+    ratio::Float64 = E1/E2
+
+    if ratio >= 1e0 # p1 has higher energy
+        w = scale*log(ratio)*rand(Float64)
+        t = acos(p1v[2])/pi
+        h = p1v[3]
+    else
+        w = scale*log(1/ratio)*rand(Float64)
+        t = acos(p2v[2])/pi
+        h = p2v[3]
+    end
+
+    #w = min(15e0,w) # limit the rapidity so that unboosted angle is not 1.0 to numerical precision. 
+
+    return (w,t,h)
+    
+end
+
+"""
+    RPointSphereWeighted!()
+
+Assigns randomly sampled angles on the sphere (cos(theta) and phi) weighed by a doppler boosting by rapidity `w`, returning the probability `prob` for such a sample and mutating the vector `a` with elements `[p, cos(theta), phi, theta]` with angles normalised by pi. 
+"""
+function RPointSphereWeighted!(a::Vector{Float64},w::Float64) 
+    # Inputs a 5 element vector [p, cos(theta), phi, theta, prob] and mutates said vector with new random values using form given in https://mathworld.wolfram.com/SpherePointPicking.html using the doppler boost formula as a weighting with a rapidity `w`. 
+    # phi points are normalised by pi
+    # prob is then the probability of sampling the random points on the sphere given the doppler boosting.
+
+    v::Float64 = rand(Float64)
+    tB::Float64 = acos(2*v-1)/pi # "boosted" theta
+    h::Float64 = 2*rand(Float64) # phi points are normalised by pi
+    t::Float64 = 2*atan(exp(-w)*tanpi(tB/2))/pi  # "unboosted" theta divided by pi
+
+    ew::Float64 = exp(w)
+    #prob::Float64 = (ew*sinpi(t/2)^2+cospi(t/2)^2/ew)^-2
+    prob::Float64 = (ew*cospi(tB/2)^2+sinpi(tB/2)^2/ew)^2
+
+    a[2] = cospi(t)
+    a[3] = h
+    a[4] = t
+
+#=     if w >= 7e0
+        println("")
+        println("w = $w")
+        println("t = $t")
+        println("tB = $tB")
+        println("h = $h")
+        println("prob = $prob")
+        println("")
+    end  =#
+
+    return prob
+    
 end
